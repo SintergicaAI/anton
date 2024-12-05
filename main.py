b54399d1a393c118092b06f594b431eb4c0984d9
edsonmanuelcarballovera <edsonmanuelcarballovera@gmail.com>
2024-12-05 10:20:57 -0600

    reorganized project for michelle only features

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
 import os
-import signal
 
 from flask import Flask, Response
 from flask_cors import CORS
-from routes.ServiceManager import docker
 
 from routes.RepositoryManager import repository_routes
-from routes.ServiceManager import service_routes
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 app.host = '0.0.0.0'
 app.port = 42000
 
-app.register_blueprint(service_routes)
 app.register_blueprint(repository_routes)
-
-
-@app.get('/kill')
-def kill():
-    docker.close()
-    os.kill(os.getpid(), signal.SIGINT)
-    return Response(
-        'Server shutting down',
-        status=200
-    )
 
 
 @app.get("/ping")
