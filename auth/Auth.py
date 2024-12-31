67253fa59087f9b8ad043b064db69073ae969bdd
edsonmanuelcarballovera <edsonmanuelcarballovera@gmail.com>
2024-12-31 05:50:36 -0600

    added basic authorization

---
+++ auth/Auth.py
@@ -0,0 +1,18 @@
+import os
+from functools import update_wrapper
+from http.client import UNAUTHORIZED
+
+from flask import request, Response
+
+
+class Auth:
+    @staticmethod
+    def requires_password(operation):
+        def wrapper(*args, **kwargs):
+            password = request.headers.get("Authorization")
+            password = password.replace("Basic ", "")
+            print(os.environ.get("PASSWORD"))
+            if password != os.environ.get("PASSWORD"):
+                return Response(status=UNAUTHORIZED)
+            return operation(*args, **kwargs)
+        return update_wrapper(wrapper, operation)
